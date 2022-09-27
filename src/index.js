9a527e1612215f83e65886aea2433a6a638eea29
nikhil.raheja287@gmail.com <nick@0308>
2022-09-27 14:12:00 +0530

    #SPG2-0000|Bug fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
-import addButton from './Pages/commonComponent/add';
-import ordersPage from './Pages/transaction/orders/order';
-import paymentPage from './Pages/transaction/payments/payment';
 
-export {  addButton,ordersPage,paymentPage }
+export { default as addButton } from './Pages/commonComponent/add';
